cad82e7b750341ab0301db5464473bba30bd2d67
kirjorjos <35607697+kirjorjos@users.noreply.github.com>
2026-09-12 05:47:36 -0400

    yes

--- src/tests/visualTransformer/errorPropagation.test.ts
+++ src/tests/visualTransformer/errorPropagation.test.ts
@@ -8,7 +8,7 @@ describe("typeMismatchDetection", () => {
     const result = steps(makeAst.typeMismatch());
     const addStep = result[2]!;
     expect(addStep.typeError).toBe(
-      "Type mismatch: expected Number, got Booleans"
+      "Type mismatch: expected Number, got Boolean"
     );
   });
 
